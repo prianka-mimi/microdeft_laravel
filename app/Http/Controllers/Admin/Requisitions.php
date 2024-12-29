2bc5f61c81c01b9bfed04f7a7da0505e22641884
Prianka Mimi <priankamimi0204@gmail.com>
2024-12-29 16:36:17 +0600

    Create another table named `requisitions` having

--- app/Http/Controllers/Admin/Requisitions.php
+++ app/Http/Controllers/Admin/Requisitions.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Assets extends Controller
+class Requisitions extends Controller
 {
     //
 }
---
+++ app/Models/Requisitions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Requisitions extends Model
+{
+    use HasFactory;
+}
